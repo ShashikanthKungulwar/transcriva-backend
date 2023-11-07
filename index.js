149eddac3e81367e771c838f62ae6341f6b0fc94
shashikanth <shashikanthk80@gmail.com>
2023-11-08 00:20:49 +0530

    setup cors

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const PORT = 8000;
 const app = express();
 const db =require('./config/mongoose_config')
 const bodyParser = require('body-parser')
+const cors=require('cors');
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 //routes
 app.use('/', require('./routes/index'));
